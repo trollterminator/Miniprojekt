a76e71118ece3c84fbec043ed1e6d7e23dea22d8
Jacob Ladegaard Friis Lageri <jaco7925@edu.eal.dk>
2025-10-09 15:18:23 +0200

    Changed gitignore paths and renamed folders

--- modul06/book-api-ef/Model/Author.cs
+++
@@ -1,9 +0,0 @@
-namespace Model
-{
-    public class Author
-    {
-        public int AuthorId { get; set; }
-        public string Fullname { get; set; }
-        public List<Book> Books { get; set; }
-    }
-}
--- modul06/book-api-ef/Model/Book.cs
+++
@@ -1,9 +0,0 @@
-namespace Model
-{
-    public class Book
-    {
-        public int BookId { get; set; }
-        public string Title { get; set; }
-        public Author Author { get; set; }
-    }
-}
