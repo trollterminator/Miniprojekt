91b4ec53abdddfaf93a054447abe1d8f79262a31
Jacob Lageri <jacob.Lageri@hotmail.com>
2025-10-19 21:49:23 +0200

    +Added some functionality for handling users being added to comments
    - missing implementation for adding posts and users?

--- TemplateAPI/kreddit/kreddit-app/Services/ApiService.cs
+++ TemplateAPI/kreddit/kreddit-app/Services/ApiService.cs
@@ -31,12 +31,12 @@ public class ApiService
         return await http.GetFromJsonAsync<Post>(url);
     }
 
-    public async Task<Comment> CreateComment(string content, int postId, int userId)
+    public async Task<Comment> CreateComment(string content, int postId, string user)
     {
         string url = $"{baseAPI}posts/{postId}/comments";
      
         // Post JSON to API, save the HttpResponseMessage
-        HttpResponseMessage msg = await http.PostAsJsonAsync(url, new { content, userId });
+        HttpResponseMessage msg = await http.PostAsJsonAsync(url, new { content, user });
 
         // Get the JSON string from the response
         string json = msg.Content.ReadAsStringAsync().Result;
